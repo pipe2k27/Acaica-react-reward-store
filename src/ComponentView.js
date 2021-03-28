782c51fd29110e7ce93bbd6cb626f558f4c554bd
Felipe Perarnau <59627664+pipe2k27@users.noreply.github.com>
2021-03-28 11:18:12 -0300

    first commit

---
+++ src/ComponentView.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Product from "./components/Product";
+
+const Components = () => {
+  return <Product />;
+};
+
+export default Components;
---
+++ src/components/Product/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="product">
+      <img
+        alt="product"
+        src="https://4.img-dpreview.com/files/p/TS600x450~sample_galleries/5471342450/2247087994.jpg"
+      />
+    </div>
+  );
+};
+
+export default Product;
